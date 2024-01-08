ce256600288109f0a80a4726d2438a006a620de8
Van Truong <van.t414@gmail.com>
2024-01-07 20:16:05 -0800

    Initial Commit

---
+++ TimeDebt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TimeDebt
+//
+//  Created by Van Truong on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeDebt/TimeDebtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeDebtApp.swift
+//  TimeDebt
+//
+//  Created by Van Truong on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeDebtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
